8b6bd0417e4269e71e7dddb238bc1d5a6b68702c
= <adahan93@gmail.com>
2013-11-09 23:39:39 -0500

    Implemented Game and Channel classes; implemented functions to: get top n channels and to get top n games being played

--- twitchDriver.php
+++ twitchDriver.php
@@ -1,5 +1,59 @@
 <?php
+	/*Error Reporting On*/
+	ini_set('display_errors',1); 
+ 	error_reporting(E_ALL);
 
-	require_once('twitchEasyAPI.php');
-	
+	require_once("twitchEasyAPI.php");
+
+	/** 	Table of top 14 Channels 	  **/
+	/***********************************/
+	$topChannels = top_n_Channels(14);
+
+	echo "<table border=\"1\">";
+		echo "<tr>";
+			echo "<th>Rank</th>";
+			echo "<th>Channel Name</th>";
+			echo "<th>Game Played</th>";
+			echo "<th>Viewer Count</th>";
+		echo "</tr>";
+
+	foreach($topChannels as $channelElement) {
+		echo "<tr>";
+		echo "<td>" . $channelElement->rank() . "</td>";
+		echo "<td>" . $channelElement->name() . "</td>";
+		echo "<td>" . $channelElement->game() . "</td>";
+		echo "<td>" . $channelElement->viewers() . "</td>";
+		echo "</tr>";
+	}
+
+    echo "</table>";
+
+
+    echo "<br><br><br>";
+
+    /** 	Table of top 10 games 	  **/
+	/***********************************/
+	$topGames = top_n_Games(10);
+
+	echo "<table border=\"1\">";
+		echo "<tr>";
+			echo "<th>Rank</th>";
+			echo "<th>Game Name</th>";
+			echo "<th>Channel Count</th>";
+			echo "<th>Viewer Count</th>";
+			echo "<th>Average Viewers per Channel</th>";
+		echo "</tr>";
+
+	foreach($topGames as $gameElement) {
+		echo "<tr>";
+		echo "<td>" . $gameElement->rank() . "</td>";
+		echo "<td>" . $gameElement->name() . "</td>";
+		echo "<td>" . $gameElement->number_of_channels() . "</td>";
+		echo "<td>" . $gameElement->viewers() . "</td>";
+		echo "<td>" . $gameElement->average_viewers_per_channel() . "</td>";
+		echo "</tr>";
+	}
+
+    echo "</table>";
+
 ?>
--- twitchEasyAPI.php
+++ twitchEasyAPI.php
@@ -1,52 +1,151 @@
 <?php
-	class Game {
-		private $game_name = "";
-		private $viewer_count = -1;
-		private $channel_count = -1;
 
-		function Game($gameName, $viewerCount, $channelCount) {
-			$game_name = $gameName;
-			$viewer_count = $viewerCount;
-			$channel_count = $channelCount;
+	class Game {
+
+		private $game_name = "";
+		private $game_viewers = -1;
+		private $channel_count = -1;
+		
+		private $game_rank = -1;
+
+		function __construct($gameName, $viewerCount, $channelCount) {
+			$this->game_name = $gameName;
+			$this->game_viewers = $viewerCount;
+			$this->game_channels = $channelCount;
+		}
+
+		function name() {
+			return $this->game_name;
+		}
+
+		function viewers() {
+			return $this->game_viewers;
+		}
+
+		function number_of_channels() {
+			return $this->game_channels;
+		}
+
+		function set_rank($gameRank) {
+			$this->game_rank = $gameRank;
+		}
+
+		function rank() {
+			return $this->game_rank;
+		}
+
+		function average_viewers_per_channel() {
+			if($this->game_channels < 0 && $this->game_viewers < 0) {
+				return 0;
+			}
+
+			else {
+				return number_format(bcdiv($this->game_viewers, $this->game_channels, 3), 2);
+			}
 		}
 
 	}
 
 	class Channel {
-		private $channelName = "";
-		private $channelViewers = -1;
-		private $channelGame = "";
+
+		private $channel_name = "";
+		private $channel_game = "";
+		private $channel_viewers = -1;
+
+		private $channel_rank = -1; //Rank from the last top_n_Channels() call.
 
 		private $smallPreviewImageURL = "";
 		private $mediumPreviewImageURL = "";
 		private $largePreviewImageURL = "";
+
+		function __construct($channelName, $channelGame, $channelViewers) {
+			$this->channel_name = $channelName;
+			$this->channel_game = $channelGame;
+			$this->channel_viewers = $channelViewers;
+		}
+
+		function name() {
+			return $this->channel_name;
+		}
+
+		function viewers() {
+			return $this->channel_viewers;
+		}
+
+		function game() {
+			return $this->channel_game;
+		}
+
+		function set_rank($rankNumber) {
+			$this->channel_rank = $rankNumber;
+		}
+
+		function rank() {
+			return $this->channel_rank;
+		}
+
+		function small_preview_image() {
+			return $this->smallPreviewImageURL;
+		}
+		
+		function medium_preview_image() {
+			return $this->mediumPreviewImageURL;
+		}
+
+		function large_preview_image() {
+			return $this->largePreviewImageURL;
+		}
+
 	}
 
 
 	/*
-	 * Pre: 0 < n_Channels <= 25
-	 * Returns top n_Channels channels
+	 * Pre: 0 < n <= 25 (OTHERWISE: return empty array)
+	 * Returns top n channels
 	 */
-	function top_n_Channels($n_Channels) {
+	function top_n_Channels($n) {
+		if($n > 0 && $n <= 25) {
+			$incrementor = 0;
+			$streams = json_decode(file_get_contents("https://api.twitch.tv/kraken/streams"));
+			$topChannels;
 
+			while($incrementor < $n) {
+				$topChannels[$incrementor] = new Channel($streams->streams[$incrementor]->channel->name, $streams->streams[$incrementor]->channel->game, $streams->streams[$incrementor]->viewers);
+				$topChannels[$incrementor]->set_rank($incrementor + 1);
+				$incrementor++;
+			}
+
+			return $topChannels;
+		}
+
+		else {
+			return array(); //Invalid paramater n --> so return an empty array
+		}
 	}
 
 
 	/**
-	 * Pre: 0 < n_Games <= 10
+	 * Pre: 0 < n <= 10
 	 * Returns top n games.
 	 */
-	function top_n_Games($n_Games) {
-		
-	}
+	function top_n_Games($n) {
+		if($n > 0 && $n <= 10) {
+			$incrementor = 0;
+			$games = json_decode(file_get_contents("https://api.twitch.tv/kraken/games/top"));
+			$topGames;
 
+			while($incrementor < $n) {
+				$topGames[$incrementor] = new Game($games->top[$incrementor]->game->name, $games->top[$incrementor]->viewers, $games->top[$incrementor]->channels);
+				$topGames[$incrementor]->set_rank($incrementor + 1);
+				$incrementor++;
+			}
 
-
-
-		$streams = json_decode(file_get_contents("https://api.twitch.tv/kraken/streams"));
-
-		foreach ($streams->streams as &$streamElement) {
-			echo "Name: <b>" . $streamElement->channel->name . "</b> Game: <b><i>" . $streamElement->channel->game . "</i></b> <br/><br/>";
+			return $topGames;
 		}
+
+		else {
+			return array(); //Invalid paramater n --> so return an empty array
+		}
+	}
 
 ?>
